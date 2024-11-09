fe956b0bb17c82a0bc6a80602f3f7fc384d47937
lbaierreinio <lucbaierreinio@gmail.com>
2024-11-09 16:10:10 -0500

    Simple sequential mode implementation

--- minGRU/minGRU.py
+++ minGRU/minGRU.py
@@ -2,8 +2,17 @@ import torch
 import torch.nn as nn
 
 class minGRU(nn.Module):
-    def __init__(self):
+    def __init__(self, dim_x, dim_h):
         super().__init__()
+        self.linear_z = nn.Linear(dim_x, dim_h)
+        self.linear_h = nn.Linear(dim_x, dim_h)
     
-    def forward(self, x):
-        return x
+    '''
+    Sequential Mode
+    '''
+    def forward(self, x_t, h_prev):
+        z_t = torch.sigmoid(self.linear_z(x_t)) # Output gate
+        h_tilde = self.linear_h(x_t) # Candidate cell
+        h_t = ((1 - z_t) * h_prev) + (z_t * h_tilde) # New hidden state
+        return h_t
+
