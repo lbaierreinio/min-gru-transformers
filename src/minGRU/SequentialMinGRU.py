f084e3ed4d87dcb4e0aa2428ef8f624b2c5dae64
lbaierreinio <lucbaierreinio@gmail.com>
2024-11-10 18:22:44 -0500

    add docstring to sequential model

--- src/minGRU/SequentialMinGRU.py
+++ src/minGRU/SequentialMinGRU.py
@@ -7,11 +7,17 @@ class SequentialMinGRU(nn.Module):
         self.linear_z = nn.Linear(dim_x, dim_h)
         self.linear_h = nn.Linear(dim_x, dim_h)
     
-    '''
-    x_t: (batch_size, input_size)
-    h_prev: (batch_size, hidden size)
-    '''
     def forward(self, x_t, h_prev):
+        """
+        Compute the forward pass.
+
+        Args:
+            x: torch.Tensor, shape (batch_size, seq_len, input_size)
+            h_prev: torch.Tensor, shape (batch_size, 1, hidden_size)
+        
+        Returns:
+            h_t: torch.Tensor, shape (batch_size, 1, hidden_size)
+        """
         z_t = torch.sigmoid(self.linear_z(x_t)) # Output gate
         h_tilde = self.linear_h(x_t) # Candidate cell
         h_t = ((1 - z_t) * h_prev) + (z_t * h_tilde) # New hidden state
