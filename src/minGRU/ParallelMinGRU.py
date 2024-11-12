12bcc49b6d98d0a0d01cbbcffc3dd7efd5a1d8fd
lbaierreinio <lucbaierreinio@gmail.com>
2024-11-11 21:45:58 -0500

    Update logic to re-incorporate h_prev

--- src/minGRU/ParallelMinGRU.py
+++ src/minGRU/ParallelMinGRU.py
@@ -14,12 +14,13 @@ class ParallelMinGRU(nn.Module):
     def log_g(self, x):
         return torch.where(x >= 0, torch.log(F.relu(x)+0.5), -F.softplus(-x))
 
-    def forward(self, x):
+    def forward(self, x, h_prev): # TODO: Figure out what h_0 is
         """
         Compute the forward pass.
 
         Args:
             x: torch.Tensor, shape (batch_size, seq_len, input_size)
+            h_prev: torch.Tensor, shape (batch_size, 1, hidden_size)
         
         Returns:
             h: torch.Tensor, shape (batch_size, seq_len, hidden_size)
@@ -28,13 +29,12 @@ class ParallelMinGRU(nn.Module):
         tilde_h = self.linear_h(x) # Candidate state
         log_z = -F.softplus(-k) # Log (z) 
         log_one_minus_z = -F.softplus(k) # Log (1 - z)
+        log_h_prev = self.log_g(h_prev) # Hidden state at time 0
         log_tilde_h = self.log_g(tilde_h) # Log candidate state
         # Parallel scan (log z + log h_tilde since we are using log, and had z * h_tilde in the original implementation)
-        h = parallel_scan_log(log_one_minus_z, log_z + log_tilde_h, dim=1)
+        h = parallel_scan_log(log_one_minus_z, torch.cat([log_h_prev, log_z + log_tilde_h], dim=1))
         """
-        Note: Currently, I am working under the assumption that
-        we return the h[1:t], because we will need all h[i] for 
-        the subsequent layer. TODO: Verify this assumption.
+        Return h[1:t] through linear layer
         """
         out = self.linear_o(h[:, 1:]) # Return h[1:t] as the output
         return out
