2e077dcfc338d3c8ca96e9f48b2e77a0212237da
lbaierreinio <lucbaierreinio@gmail.com>
2024-11-12 09:48:44 -0500

    update test_mingru

--- tests/test_mingru.py
+++ tests/test_mingru.py
@@ -2,10 +2,11 @@ from src.minGRU.ParallelMinGRU import ParallelMinGRU
 import torch
 class TestMinGRU:
     def test_min_gru(self):
-        parallel_min_gru = ParallelMinGRU(2, 3)
-        x = torch.randn((1, 2, 2))
-        h_0 = torch.randn((1, 1, 3))
+        batch_size, seq_len, input_size, hidden_size = 1, 2, 2, 3
+        parallel_min_gru = ParallelMinGRU(input_size, hidden_size)
+        x = torch.randn((batch_size, seq_len, input_size))
+        h_0 = torch.randn((batch_size, 1, hidden_size))
 
         h_parallel = parallel_min_gru(x, h_0)
 
-        assert h_parallel.shape == (1, 2, 2)
+        assert h_parallel.shape == (batch_size, seq_len, input_size)
