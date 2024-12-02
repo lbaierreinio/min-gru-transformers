dafa0ce364e4bb378160665a12167603529a6768
lbaierreinio <lucbaierreinio@gmail.com>
2024-12-01 23:52:21 -0500

    revert changes to BiMinGRU

--- src/layers/rnn/BiMinGRU.py
+++ src/layers/rnn/BiMinGRU.py
@@ -2,19 +2,18 @@ import torch
 from torch import nn
 from layers.rnn.MinGRU import MinGRU
 
-
 class BiMinGRU(nn.Module):
-    def __init__(self, dim_in, dim_hidden):
-        super().__init__()
-        self.forward_rnn = MinGRU(dim_in, dim_hidden)
-        self.backward_rnn = MinGRU(dim_in, dim_hidden)
-        self.linear = nn.Linear(2 * dim_hidden, dim_hidden)
+  def __init__(self, dim_in, dim_hidden):
+    super().__init__()
+    self.forward_rnn = MinGRU(dim_in, dim_hidden)
+    self.backward_rnn = MinGRU(dim_in, dim_hidden)
+    self.linear = nn.Linear(2 * dim_hidden, dim_hidden)
+  
+  def forward(self, x):
+    x_reversed = x.flip(dims=[1])
+    out_forward = self.forward_rnn(x)
+    out_backward = self.backward_rnn(x_reversed)
+    concat = torch.cat((out_forward, out_backward.flip(dims=[1])), dim=2)
+    out = self.linear(concat)
 
-    def forward(self, x):
-        x_reversed = x.flip(dims=[1])
-        out_forward = self.forward_rnn(x)
-        out_backward = self.backward_rnn(x_reversed)
-        concat = torch.cat((out_forward, out_backward), dim=2)
-        out = self.linear(concat)
-
-        return out
+    return out
