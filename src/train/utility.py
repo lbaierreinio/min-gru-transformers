10a8bfddca00158c3515e7313608d5d637f79daf
Luc Gabriel Baier-Reinio <lucbr@mail.cs.toronto.edu>
2024-12-04 11:43:22 -0500

    add avg time per epoch

--- src/train/utility.py
+++ src/train/utility.py
@@ -11,7 +11,8 @@ def evaluate(model, dataloader, loss_fn, evaluation_type='Validation'):
         for batch in dataloader:
             input = batch['input_ids'].to(device)
             labels = batch['labels'].to(device)
-            output = model(input)
+            mask = ~batch['attention_mask'].to(device).bool()
+            output = model(input, mask)
 
             # Total loss
             loss = loss_fn(output, labels)
@@ -28,12 +29,12 @@ def evaluate(model, dataloader, loss_fn, evaluation_type='Validation'):
         return total_loss, accuracy
 
 
-def train(model, train_dataloader, val_dataloader, num_epochs, loss_fn, learning_rate, *, early_stopping_threshold=None):
+def train(model, train_dataloader, val_dataloader, num_epochs, loss_fn, learning_rate, *, early_stopping_threshold=None, check_every_i=1):
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     steps = 0
     total_time = 0
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    for epoch in range(1, num_epochs+1):
+    for epoch in range(0, num_epochs):
         model.train()
         total_loss = 0.0
         for batch in train_dataloader:
@@ -41,30 +42,32 @@ def train(model, train_dataloader, val_dataloader, num_epochs, loss_fn, learning
             labels = batch['labels'].to(device)
             mask = ~batch['attention_mask'].to(device).bool()
 
-            start = time.time()  # TODO: Verify this is a good way to measure time b/c batch size might not necessarily always be the same(?)
+            t0 = time.time()
+            # Step
             optimizer.zero_grad()
             output = model(input, mask=mask)
             loss = loss_fn(output, labels)
-            total_loss += loss.item()
+            total_loss += loss.detach()
             loss.backward()
             optimizer.step()
+            
             if torch.cuda.is_available():
                 torch.cuda.synchronize()
-            end = time.time()
-            total_time += (end - start)
+            
+            total_time += (time.time() - t0)
+            
             steps += 1
 
-        # Naive Early Stopping (TODO: Revisit if this is a good idea & also consider checking every Epoch for improved accuracy)
-        if epoch % 5 == 0:
+        if (epoch + 1) % check_every_i == 0:
             print(f"------------ EPOCH {epoch} ------------ \n")
             print(f"Training Loss: {round(total_loss, 4)}")
             total_loss, accuracy = evaluate(
                 model, val_dataloader, loss_fn, 'Validation')
             if early_stopping_threshold is not None and accuracy >= early_stopping_threshold:
                 print(f"Early stopping at epoch {epoch}")
-                return total_loss, accuracy, steps, epoch, (total_time / steps)
+                return total_loss, accuracy, steps, epoch, (total_time / num_epochs)
             print('\n\n')
 
     total_loss, accuracy = evaluate(
         model, val_dataloader, loss_fn, 'Validation')
-    return total_loss, accuracy, steps, num_epochs, (total_time / steps)
+    return total_loss, accuracy, steps, num_epochs, (total_time / epoch)
