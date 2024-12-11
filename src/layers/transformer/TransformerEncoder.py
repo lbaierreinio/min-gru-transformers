6ed57136aaddae02f45b83bf566b884dfb013336
Luc Gabriel Baier-Reinio <lucbr@mail.cs.toronto.edu>
2024-12-10 19:43:48 -0500

    Implement fix

--- src/layers/transformer/TransformerEncoder.py
+++ src/layers/transformer/TransformerEncoder.py
@@ -5,7 +5,31 @@ from layers.transformer.PositionalEncoding import PositionalEncoding
 from layers.transformer.TransformerEncoderBlock import TransformerEncoderBlock
 
 class TransformerEncoder(nn.Module):
+    """
+    The Transformer Encoder. Responsible for embedding, positional encoding, chunking,
+    stacking multiple TransformerEncoderBlocks, and outputting the final hidden state
+    following aggregation of chunks by a MinGRU layer.
+    """
     def __init__(self, vocab_size, num_heads, num_layers, num_hiddens, ffn_num_hiddens, dropout, max_len, chunk_size=None):
+        """
+        Args:
+            vocab_size: int
+                The size of the vocabulary.
+            num_heads: int
+                The number of attention heads.
+            num_layers: int
+                The number of transformer layers.
+            num_hiddens: int
+                The number of hidden units.
+            ffn_num_hiddens: int
+                The number of hidden units in the feedforward network.
+            dropout: float
+                The dropout probability.
+            max_len: int
+                The maximum sequence length.
+            chunk_size: int
+                The chunk size for chunked attention. Default is None.
+        """
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, num_hiddens)
         self.chunk_size = chunk_size
@@ -20,8 +44,8 @@ class TransformerEncoder(nn.Module):
     def forward(self, x, mask=None):
         """
         Args:
-            src: Tensor of shape [batch_size, seq_len]
-            mask: ByteTensor of shape [batch_size, seq_len]
+            x: Tensor (batch_size, seq_len)
+            mask (optional): Tensor (batch_size, seq_len)
         """
         # Embedding
         x = self.embedding(x) * math.sqrt(self.num_hiddens)
@@ -49,7 +73,8 @@ class TransformerEncoder(nn.Module):
             
             chunk_indices = [(i+1)*self.chunk_size-1 for i in range(0, num_chunks)]
             x = x[:, chunk_indices]
-            mask = mask[:, chunk_indices]
+            if mask is not None:
+                mask = mask[:, chunk_indices]
 
             x = self.min_gru_out(x, mask=mask) # (B, N * C, H)
 
