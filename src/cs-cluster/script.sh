e11cb99474b5a6392cc3e4cb925be4a723fd22e4
Luc Gabriel Baier-Reinio <lucbr@mail.cs.toronto.edu>
2024-12-11 14:30:07 -0500

    add chunk_size as config

--- src/cs-cluster/script.sh
+++ src/cs-cluster/script.sh
@@ -2,4 +2,4 @@
 source /w/340/lucbr/miniconda3/bin/activate
 export PYTHONPATH="$PYTHONPATH:/w/331/lucbr/min-gru-transformers/src"
 conda activate pytorch_env
-python3 -u experiments/train_models.py --out_path='both_models_1024_4000_final.csv' --mingru_dataset_path='mingru_1024_4000_final.pt' --transformer_dataset_path='transformer_512_4000_final.pt'
+python3 -u experiments/train_models.py --out_path='both_models_1024_4000_final.csv' --mingru_dataset_path='mingru_1024_4000_random.pt' --transformer_dataset_path='transformer_512_4000_random.pt'
